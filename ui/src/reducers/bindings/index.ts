8d4d98b61d62351ab7a5b4076323a5a0a3b72e2c
Ricardo Olsen <olsen.ricardo@gmail.com>
2023-09-21 08:58:36 -0300

    Add files via upload

--- ui/src/reducers/bindings/index.ts
+++ ui/src/reducers/bindings/index.ts
@@ -1,4 +1,4 @@
-import produce from 'immer'
+import { produce } from 'immer';
 
 import { BindingActions, BindingState, BindingActionTypes } from './types'
 import { InputActions, InputState } from '../inputs/types'
